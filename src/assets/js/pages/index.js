3d3cbbd629a68695b5219bf0496db9cde0f3aca8
GoldenDragon0705 <GoldenDragon0705@outlook.com>
2023-11-03 06:25:07 -0400

    add filter to open only image files

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -148,6 +148,9 @@ $(() => {
       const dialogConfig = {
         title: 'Select image files.',
         buttonLabel: 'Select',
+        filters: [{
+          name: "Image files", extensions: ["jpg", "jpeg", "png"]
+        }],
         properties: ['openFile', 'multiSelections']
       };
       try {
